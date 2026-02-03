3842dc77fe9e605110a3972c3f092d0d11cff671
Kirill <dawiduk2013@yandex.ru>
2026-02-03 17:24:56 +0500

    Add files via upload

---
+++ lesson_4/Avatar.py
@@ -0,0 +1,9 @@
+#from PIL import Image
+
+#image = Image.open(r"D:\Python\python_scripts\Основы_Python\strawberry.jpeg")
+#rotated_image = image.rotate(45)
+#rotated_image.save("rotated_strawberry.jpeg")
+
+#print(image.widht, image.height, image.mode, sep='\n')
+
+print(600,1200,"CMYK", sep='\n')
