4a7afd28087ebb7cd7c0f0f1267968846c94c4f6
Kirill <dawiduk2013@yandex.ru>
2026-02-17 13:22:16 +0500

    Update Passwords.py

--- lesson_6/Passwords.py
+++ lesson_6/Passwords.py
@@ -1,11 +1,19 @@
 def is_very_long(password):
-    return len(password)>12
+    return len(password) > 12
+
+
 def has_digit(password):
     return any(i.isdigit() for i in password)
+
+
 def has_upper_letters(password):
     return any(i.isupper() for i in password)
+
+
 def has_lower_letters(password):
     return any(i.islower() for i in password)
+
+
 def has_symbols(password):
     return any(not i.isdigit() and not i.isalpha() for i in password)
 
@@ -14,14 +22,14 @@ def main():
     password = input("Введите пароль: ")
     score = 0
     verification = [
-        is_very_long(password),
-        has_digit(password),
-        has_upper_letters(password),
-        has_lower_letters(password),
-        has_symbols(password)
+        is_very_long,
+        has_digit,
+        has_upper_letters,
+        has_lower_letters,
+        has_symbols
     ]
     for v in verification:
-        if v:
+        if v(password):
             score += 2
     print("Рейтинг пароля: ", score)
 
